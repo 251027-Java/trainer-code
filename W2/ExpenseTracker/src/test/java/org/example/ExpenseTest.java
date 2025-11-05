a4e61c2b9f95e715bc5a0e6580d7710e2555f2b1
Richard Hawkins <Richard.Hawkins@Revature.com>
2025-11-05 10:42:08 -0600

    add chinook

--- W2/ExpenseTracker/src/test/java/org/example/ExpenseTest.java
+++ W2/ExpenseTracker/src/test/java/org/example/ExpenseTest.java
@@ -2,12 +2,15 @@ package org.example;
 
 import java.util.Date;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ExpenseTest {
     public static void main(String[] args){
-        testExpenseCreation();
+    //    testExpenseCreation();
     }
 
-    public static void testExpenseCreation() {
+    public static void testManualExpenseCreation() {
         // Arrange the conditions for your test
         Expense expense = new Expense(1, new Date(), 100, "DummyMerchant");
 
@@ -19,7 +22,16 @@ public class ExpenseTest {
         if (possibleID != 1) {System.out.println("Failed id");}
         else if (possibleValue != 100) {System.out.println("Failed value");}
         else { System.out.println("Passed expense creation!");}
-
     }
 
+    @Test
+    public void testExpenseCreation(){
+        // Arrange:
+        Expense expense = new Expense(1, new Date(), 100, "Dummy Merchant");
+
+        // Act: ( the arrange was also most of the acting too)
+        // Assert:
+        assertEquals(1, expense.getId());
+        assertEquals(100, expense.getValue());
+    }
 }
