3d7c22532a8214d0c8d5922d40fef16582aa6942
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-11-25 14:17:36 -0500

    almost working hardcoded login

--- W5/Ben-Demos/pokemon-hunter/src/app/components/login/login.component.ts
+++ W5/Ben-Demos/pokemon-hunter/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  //Depedency Inject Router to switch URLS
+  constructor(private router:Router){}
+
+  //Variables that will hold the username/password
+  username:string = ""
+  password:string = ""
+
+  //Hardcoded login for now
+  login(){
+    if(this.username === "user" && this.password === "password"){
+      //Switch URLs within Typescript using Router
+      this.router.navigateByUrl("/dashboard")
+    }
+  }
 
 }
