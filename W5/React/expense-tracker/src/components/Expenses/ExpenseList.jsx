8c2ddac01e3815032a1eed3d3827bb5d4da0567a
Richard Hawkins <Richard.Hawkins@Revature.com>
2025-11-25 11:50:13 -0600

    Add expense reports

--- W5/React/expense-tracker/src/components/Expenses/ExpenseList.jsx
+++ W5/React/expense-tracker/src/components/Expenses/ExpenseList.jsx
@@ -1,6 +1,6 @@
 import ExpenseItem from "./ExpenseItem";
 
-const ExpenseList = ({ items }) => {
+const ExpenseList = ({ items, selectedIds, onToggleItem }) => {
     if (items.length === 0) {
         return <h2 className = "text-2xl bg-slate-200 p-6 m-auto rounded-2xl shadow-inner font-bold text-slate-800">No expenses found.</h2>;
     };
@@ -10,9 +10,12 @@ const ExpenseList = ({ items }) => {
             { items.map ((expense) => (
                 <ExpenseItem 
                     key = {expense.id}
+                    id = {expense.id}
                     title = {expense.title}
                     amount = {expense.amount}
-                    date = {expense.date} />
+                    date = {expense.date} 
+                    isSelected = {selectedIds.includes(expense.id)}
+                    onToggle= {onToggleItem}/>
             )) }
         </div>
     );
