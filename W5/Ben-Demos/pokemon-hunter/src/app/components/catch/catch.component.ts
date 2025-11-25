590b8416b6ce9e389edcdd09489927f3c6d554dd
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-11-25 15:52:55 -0500

    random pokemon appears in catch component

--- W5/Ben-Demos/pokemon-hunter/src/app/components/catch/catch.component.ts
+++ W5/Ben-Demos/pokemon-hunter/src/app/components/catch/catch.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PokemonService } from '../../services/pokemon.service';
 
 @Component({
   selector: 'app-catch',
@@ -7,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrl: './catch.component.css'
 })
 export class CatchComponent {
+
+  //Constructor Inject the Service
+  constructor(private pokemonService:PokemonService){} 
+
+  //Variable to hold the pokemon from PokeAPI
+  pokemon:any = {}
+
+  //ngOnInit - a component lifecycle method that lets us define logic to fire when the component renders
+  ngOnInit(){
+    this.getPokemon()
+  }
+
+  //The method that gets a Pokemon with the Service
+  getPokemon(){
+
+    //When we get an Observable returned, we need to SUBSCRIBE to it to access its data (The HTTP Response)
+    this.pokemonService.getPokemon().subscribe(data => {
+      console.log(data)
+
+      //populate pokemon with the data in the response
+      this.pokemon = data
+    })
+
+  }
 
 }
