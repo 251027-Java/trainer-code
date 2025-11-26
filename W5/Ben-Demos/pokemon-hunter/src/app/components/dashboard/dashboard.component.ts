c3d2465fc8c34142702426d36b3b976638600878
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-11-26 11:15:12 -0500

    navbar works!

--- W5/Ben-Demos/pokemon-hunter/src/app/components/dashboard/dashboard.component.ts
+++ W5/Ben-Demos/pokemon-hunter/src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { PokemonService } from '../../services/pokemon.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+
+  //Constructor Inject the Service
+  constructor(public pokemonService:PokemonService){}
 
 }
