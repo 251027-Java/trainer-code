1d056814c136a671cb0771b9da4a3ca1f5e2096e
Richard Hawkins <Richard.Hawkins@Revature.com>
2025-12-04 14:40:25 -0600

    finished basic auth example

--- W6/ExpenseReport/src/main/java/com/revature/ExpenseReport/ExpenseReportApplication.java
+++ W6/ExpenseReport/src/main/java/com/revature/ExpenseReport/ExpenseReportApplication.java
@@ -1,7 +1,11 @@
 package com.revature.ExpenseReport;
 
+import com.revature.ExpenseReport.Model.AppUser;
 import com.revature.ExpenseReport.Model.Expense;
+import com.revature.ExpenseReport.Model.Report;
+import com.revature.ExpenseReport.Repository.AppUserRepository;
 import com.revature.ExpenseReport.Repository.ExpenseRepository;
+import com.revature.ExpenseReport.Repository.ReportRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,13 +23,59 @@ public class ExpenseReportApplication {
 	}
 
     @Bean // Bean is a single method that is run after the application is started
-    CommandLineRunner seedData (ExpenseRepository repository) {
+    CommandLineRunner seedData (ExpenseRepository expenseRepository, ReportRepository reportRepository, AppUserRepository appUserRepository) {
         return args -> {
-            var e1 = new Expense(LocalDate.now(), new BigDecimal(59.99), "Walmart");
-            var e2 = new Expense(LocalDate.now().minusDays(1), new BigDecimal(14.75), "Starbucks");
-            var e3 = new Expense(LocalDate.now().minusDays(2), new BigDecimal(99.88), "Buffalo Wild Wings");
 
-            repository.saveAll(List.of(e1, e2, e3));
+            // Report seed
+            var r1 = new Report("Plano", "DRAFT");
+            var r2 = new Report("Reston", "SUBMITTED");
+            reportRepository.save(r1);
+            reportRepository.save(r2);
+
+            // Expenses seed
+            var e1 = new Expense(LocalDate.now(), new BigDecimal(59.99), "Walmart");
+            e1.setReport(r1);
+
+            var e2 = new Expense(LocalDate.now().minusDays(1), new BigDecimal(14.75), "Starbucks");
+            e2.setReport(r1);
+
+            var e3 = new Expense(LocalDate.now().minusDays(25), new BigDecimal(99.88), "Buffalo Wild Wings");
+            e3.setReport(r2);
+
+            expenseRepository.saveAll(List.of(e1, e2, e3));
+
+            // AppUser seed
+            appUserRepository.save(new AppUser("admin", "password123", "ADMIN"));
+            appUserRepository.save(new AppUser("user", "secret", "USER"));
         };
     }
 }
+
+
+
+
+// Params
+// report id, expense id
+
+
+// verify they exist in the database
+// retrieve from the db
+// is the expense already on a report?
+//  - if it is...?
+//      - stop the action, alert the user
+//  - if it's not...?
+//      -attach the expense to the report, and the report id to the expense
+
+// save to the db
+
+
+
+
+
+
+
+
+
+
+
+
