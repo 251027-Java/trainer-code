c6c4a126bcb57293cdbd0d3da8c386d117931871
snsorl <suryasolaiappan@gmail.com>
2025-12-09 12:01:04 -0600

    adding ReportTest

---
+++ W7/ExpenseReport/src/test/java/com/revature/ExpenseReport/Service/ReportServiceTests.java
@@ -0,0 +1,68 @@
+package com.revature.ExpenseReport.Service;
+
+import com.revature.ExpenseReport.Controller.ExpenseDTO;
+import com.revature.ExpenseReport.Controller.ReportDTO;
+import com.revature.ExpenseReport.Model.Expense;
+import com.revature.ExpenseReport.Model.Report;
+import com.revature.ExpenseReport.Repository.ExpenseRepository;
+import com.revature.ExpenseReport.Repository.ReportRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+
+@ExtendWith(MockitoExtension.class)
+public class ReportServiceTests {
+
+    // Fields
+    @Mock
+    private ExpenseRepository expenseRepository;
+
+    @Mock
+    private ReportRepository reportRepository;
+
+    @InjectMocks
+    private ReportService reportService;
+
+    @Test
+    void happyPath_getReportById_returnsExpenseDTO() {
+
+        String expenseId = "thisIsTheId";
+        LocalDate date = LocalDate.now();
+        Expense savedExpense = new Expense(date, new BigDecimal("50.00"), "Video Games");
+        savedExpense.setId(expenseId);
+
+        ExpenseDTO expectedExpense = new ExpenseDTO(expenseId, date, new BigDecimal("50.00"), "Video Games");
+        List<ExpenseDTO> expenses = List.of(expectedExpense);
+
+        String reportId = "thisIsTheReportId";
+        Report report = new Report("Groceries", "Accepted");
+        report.setReportId(reportId);
+
+        ReportDTO expectedReport = new ReportDTO(reportId, "Groceries", "Accepted", expenses);
+
+        when(reportRepository.findById(reportId)).thenReturn(Optional.of(report));
+
+        ReportDTO actualReport = reportService.getById(reportId);
+
+
+        assertThat(actualReport).isEqualTo(expectedReport);
+    }
+
+}
+
+
+
+
