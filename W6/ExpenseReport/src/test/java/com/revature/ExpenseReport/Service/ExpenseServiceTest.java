b114e9f6a9ed45eefbebd057175b4d663f66002a
OmarH <omarhammad75@gmail.com>
2025-12-09 10:17:54 -0600

    made changes

---
+++ W6/ExpenseReport/src/test/java/com/revature/ExpenseReport/Service/ExpenseServiceTest.java
@@ -0,0 +1,24 @@
+package com.revature.ExpenseReport.Service;
+
+
+@ExtendWith(MockitoExtension.class)
+public class ExpenseServiceTest{
+
+    //Fields
+    @Mock
+    private ExpenseRepository repo;
+
+
+    //Constructors
+
+
+    //Methods
+
+
+
+        //Arrange - prepping any resources/objects we need to run our test
+
+        //Act- the function /action of executing the code/logic were testing
+
+        //Assert - the final check to pass or fail
+}
