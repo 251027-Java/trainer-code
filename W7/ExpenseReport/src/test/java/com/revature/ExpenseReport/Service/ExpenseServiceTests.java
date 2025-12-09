23775a98ce174089ce560a8e5827985a20b055a9
OmarH <omarhammad75@gmail.com>
2025-12-09 11:55:30 -0600

    delete by expense id happy path

--- W7/ExpenseReport/src/test/java/com/revature/ExpenseReport/Service/ExpenseServiceTests.java
+++ W7/ExpenseReport/src/test/java/com/revature/ExpenseReport/Service/ExpenseServiceTests.java
@@ -65,6 +65,20 @@ public class ExpenseServiceTests {
         // Assert
         // compare expected to actual
         assertThat(actual).isEqualTo(expected);
+
+        @Test
+        public void deleteExpense_HappyPath() {
+            // Arrange
+            Mockito.doNothing().when(expenseRepository).deleteById(id);
+
+            // ACT
+            expenseService.deleteExpense(id);
+
+            // Assert
+            Mockito.verify(expenseRepository, Mockito.times(1))
+                    .deleteById(id);
+        }
+
     }
 }
 
