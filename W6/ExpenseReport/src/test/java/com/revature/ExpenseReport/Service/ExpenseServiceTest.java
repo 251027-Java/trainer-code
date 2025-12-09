d0901439ce7758c16e289f10020ec83fd4b0535e
OmarH <omarhammad75@gmail.com>
2025-12-09 10:56:28 -0600

    update

--- W6/ExpenseReport/src/test/java/com/revature/ExpenseReport/Service/ExpenseServiceTest.java
+++ W6/ExpenseReport/src/test/java/com/revature/ExpenseReport/Service/ExpenseServiceTest.java
@@ -27,4 +27,29 @@ public class ExpenseServiceTest{
 
         //Assert - the final check to pass or fail
 
+
+    /*
+    public ExpenseDTO getById(String id){
+        Optional<Expense> res = repository.findById(id);
+
+        return (res.isEmpty()) ? null : ExpenseToDto(res.get());
+
+    }
+
+    */
+
+    void happyPath_getExpenseId_returnsExpenseDTO(){
+        //Arrange
+        String id = "thisIsTheId";
+        Expense savedExpense = new Expense(LocalDate.now(), new BigDecimal("50.00"), "Video Games");
+        savedExpense.setId(id);
+
+        ExpenseDTO expected = new ExpenseDTO(id, date, new BigDecimal("50.00"), "Video Games");
+
+
+        // ACT
+        service.getById(id);
+
+
+    }
 }
