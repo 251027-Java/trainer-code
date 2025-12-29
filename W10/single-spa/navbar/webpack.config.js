bc201746c40ea9133e1210368e716109a51afb3f
Richard Hawkins <Richard.Hawkins@Revature.com>
2025-12-29 02:44:05 -0600

    add W10 and Single-SPA

---
+++ W10/single-spa/navbar/webpack.config.js
@@ -0,0 +1,27 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "revature",
+    projectName: "navbar",
+    webpackConfigEnv,
+    argv,
+    outputSystemJS: true,
+  });
+
+  return merge(defaultConfig, {
+    devServer: {
+      port: 8081,
+    },
+    // Externalize React - use shared version from import map
+    externals: [
+      "react",
+      "react-dom",
+      "react-dom/client",
+      "react/jsx-runtime",
+      "react/jsx-dev-runtime",
+      "single-spa-react",
+    ],
+  });
+};
